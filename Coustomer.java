5ce87ee30e658e4654c6331f4357e65e698fb432
717824y146-star <717824y146@kce.ac.in>
2025-11-06 22:40:52 +0530

    Create Coustomer.java

---
+++ Coustomer.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Customer {
+    private final String id;
+    private String name;
+    private String address;
+
+    public Customer(String id, String name, String address){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId(){ return id; }
+    public String getName(){ return name; }
+    public void setName(String name){ this.name = name; }
+    public String getAddress(){ return address; }
+    public void setAddress(String address){ this.address = address; }
+
+    @Override public String toString(){
+        return String.format("Customer[%s] %s, %s", id, name, address);
+    }
+}
